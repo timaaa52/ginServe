ef339a028c07cb4a9e9a8c15334e22a2064c949b
Tim <haukusha1@gmail.com>
2026-07-04 00:54:07 +0300

    create serve

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Request struct {
+	Ping string `json:"ping" binding:"required"`
+}
+
+func main() {
+	route := gin.Default()
+
+	route.POST("/post", func(c *gin.Context) {
+		var req Request
+		if err := c.ShouldBindJSON(&req); err != nil {
+			return
+		}
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"pong": "0",
+		})
+	})
+	route.Run("localhost:8080")
+
+}
