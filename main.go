335b3fc6663df56ddb78fb9526293104aca2679f
Tim <haukusha1@gmail.com>
2026-07-04 00:57:35 +0300

    create err for user, when he input incorrect value

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	route.POST("/post", func(c *gin.Context) {
 		var req Request
 		if err := c.ShouldBindJSON(&req); err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 			return
 		}
 		c.IndentedJSON(http.StatusOK, gin.H{
