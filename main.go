93cc93a860aab9930742648f8322a44a25d0f9ae
Tim <haukusha1@gmail.com>
2026-07-05 22:49:45 +0300

    create struct for todo. add todo arr, write get handler

--- main.go
+++ main.go
@@ -6,23 +6,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Request struct {
-	Ping string `json:"ping" binding:"required"`
+type Todo struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	IsDone    bool   `json:"isdone"`
+	AddedDate string `json:"addeddate"`
 }
 
+var todos = []Todo{
+	{ID: "1", Title: "buy bread", IsDone: false, AddedDate: "2026-07-05"},
+	{ID: "2", Title: "feed cat", IsDone: true, AddedDate: "2026-07-03"},
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+                          
 func main() {
 	route := gin.Default()
+	route.GET("/", getTodos)
 
-	route.POST("/post", func(c *gin.Context) {
-		var req Request
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.IndentedJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
-			return
-		}
-		c.IndentedJSON(http.StatusOK, gin.H{
-			"pong": "0",
-		})
-	})
 	route.Run("localhost:8080")
 
 }
+
+// route.POST("/post", func(c *gin.Context) {
+// 	var req Request
+// 	if err := c.ShouldBindJSON(&req); err != nil {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+// 		return
+// 	}
+// 	c.IndentedJSON(http.StatusOK, gin.H{
+// 		"pong": "0",
+// 	})
+// })
