651e15def87cfadc3a57a2f7bd8a9a61e9b4226c
Tim <haukusha1@gmail.com>
2026-07-14 22:49:39 +0300

    get all ltodos from postgres using pgx

--- main.go
+++ main.go
@@ -1,138 +1,114 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"simplehttpserve/models"
-	"slices"
-
+	"simplehttpserve/services"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5"
 )
 
+// var todos = []models.Todo{
+// 	{ID: "1", Title: "buy bread", IsDone: false, AddedDate: "2026-07-05"},
+// 	{ID: "2", Title: "feed cat", IsDone: true, AddedDate: "2026-07-03"},
+// }
 
-var todos = []models.Todo{
-	{ID: "1", Title: "buy bread", IsDone: false, AddedDate: "2026-07-05"},
-	{ID: "2", Title: "feed cat", IsDone: true, AddedDate: "2026-07-03"},
-}
 
-func getTodos(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, todos)
-}
+// func addTodo(c *gin.Context) {
+// 	var todo models.Todo
+// 	if err := c.BindJSON(&todo); err != nil {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{
+// 			"err": err.Error(),
+// 		})
+// 		return
+// 	}
+// 	todos = append(todos, todo)
+// 	c.IndentedJSON(http.StatusCreated, todos)
+// }
 
-func addTodo(c *gin.Context) {
-	var todo models.Todo
-	if err := c.BindJSON(&todo); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"err": err.Error(),
-		})
-		return
-	}
-	todos = append(todos, todo)
-	c.IndentedJSON(http.StatusCreated, todos)
-}
+// func deletToDo(c *gin.Context) {
+// 	id := c.Param("id")
 
-func deletToDo(c *gin.Context) {
-	id := c.Param("id")
+// 	newTodos := slices.DeleteFunc(todos, func(t models.Todo) bool {
+// 		return t.ID == id
+// 	})
+// 	c.IndentedJSON(http.StatusOK, gin.H{
+// 		"message": "element with id:" + id + " was deleted",
+// 	})
 
-	newTodos := slices.DeleteFunc(todos, func(t models.Todo) bool {
-		return t.ID == id
-	})
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"message": "element with id:" + id + " was deleted",
-	})
+// 	todos = newTodos
+// }
 
-	todos = newTodos
-}
+// func patchToDo(c *gin.Context) {
+// 	type Param struct {
+// 		Title  *string `json:"title"`
+// 		IsDone *bool   `json:"isdone"`
+// 	}
+// 	id := c.Param("id")
+// 	var params Param
 
-func patchToDo(c *gin.Context) {
-	type Param struct {
-		Title  *string `json:"title"`
-		IsDone *bool   `json:"isdone"`
-	}
-	id := c.Param("id")
-	var params Param
+// 	if err := c.ShouldBindJSON(&params); err != nil {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{
+// 			"err": err.Error(),
+// 		})
+// 		return
+// 	}
+// 	for i, r := range todos {
+// 		if r.ID == id {
+// 			if params.IsDone != nil {
+// 				todos[i].IsDone = *params.IsDone
+// 			}
+// 			if params.Title != nil {
+// 				todos[i].Title = *params.Title
+// 			}
+// 			c.IndentedJSON(http.StatusOK, gin.H{
+// 				"message": "toDo with id " + id + " has been update",
+// 			})
+// 			return
+// 		}
+// 	}
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{
+// 		"message": "user not found",
+// 	})
 
-	if err := c.ShouldBindJSON(&params); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"err": err.Error(),
-		})
-		return
-	}
-	for i, r := range todos {
-		if r.ID == id {
-			if params.IsDone != nil {
-				todos[i].IsDone = *params.IsDone
-			}
-			if params.Title != nil {
-				todos[i].Title = *params.Title
-			}
-			c.IndentedJSON(http.StatusOK, gin.H{
-				"message": "toDo with id " + id + " has been update",
-			})
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{
-		"message": "user not found",
-	})
+// }
 
-}
+// func putToDo(c *gin.Context) {
+// 	type PutParams struct {
+// 		Title  string `json:"title" binding:"required"`
+// 		IsDone bool   `json:"isdone" binding:"required"`
+// 	}
+// 	id := c.Param("id")
+// 	var params PutParams
 
-func putToDo(c *gin.Context) {
-	type PutParams struct {
-		Title  string `json:"title" binding:"required"`
-		IsDone bool   `json:"isdone" binding:"required"`
-	}
-	id := c.Param("id")
-	var params PutParams
+// 	if err := c.ShouldBindJSON(&params); err != nil {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{
+// 			"err": err.Error(),
+// 		})
+// 		return
+// 	}
 
-	if err := c.ShouldBindJSON(&params); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"err": err.Error(),
-		})
-		return
-	}
-
-	for i, r := range todos {
-		if r.ID == id {
-			todos[i].Title = params.Title
-			todos[i].IsDone = params.IsDone
-			c.IndentedJSON(http.StatusOK, gin.H{
-				"message": "todo has been update",
-			})
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{
-		"error": "user with id " + id + " not found :(",
-	})
-}
+// 	for i, r := range todos {
+// 		if r.ID == id {
+// 			todos[i].Title = params.Title
+// 			todos[i].IsDone = params.IsDone
+// 			c.IndentedJSON(http.StatusOK, gin.H{
+// 				"message": "todo has been update",
+// 			})
+// 			return
+// 		}
+// 	}
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{
+// 		"error": "user with id " + id + " not found :(",
+// 	})
+// }
 
 func main() {
-	url := "postgres://postgres:qwerty@localhost:5432/tododb"
-
-	conn, err := pgx.Connect(context.Background(), url)
-	if err != nil { 
-		panic(err.Error())
-	}
-
-	defer conn.Close(context.Background())
-	var title string
-
-	err = conn.QueryRow(context.Background(), "select title from tododb where isdone=$1", true).Scan(&title)
-	if err != nil { 
-		fmt.Println("QueryRow Failed", err.Error())
-	}
-
-	fmt.Println(title)
-
-	route := gin.Default()
-	route.GET("/", getTodos)
-	route.POST("/todos", addTodo)
-	route.DELETE("/todos/:id", deletToDo)
-	route.PATCH("/todos/:id", patchToDo)
-	route.PUT("/todos/:id", putToDo)
+	
+	
+	// route := gin.Default()
+	// route.GET("/", services.GetTodos)
+	// route.POST("/todos", addTodo)
+	// route.DELETE("/todos/:id", deletToDo)
+	// route.PATCH("/todos/:id", patchToDo)
+	// route.PUT("/todos/:id", putToDo)
 	// route.Run("localhost:8080")
+	services.GetTodos(&gin.Context{})
 }
