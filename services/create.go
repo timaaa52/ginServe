ed70e050967030da502597097ffd1d10271dd2c1
Tim <haukusha1@gmail.com>
2026-07-19 15:55:12 +0300

    add service for create a todo

---
+++ services/create.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"context"
+	"net/http"
+	"simplehttpserve/database"
+	"simplehttpserve/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CraeteTodo(c *gin.Context) {
+
+	db, err := database.Connection()
+	if err != nil { 
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	var todo models.Todo 
+	if err := c.BindJSON(&todo); err != nil { 
+		c.IndentedJSON(http.StatusBadRequest, gin.H{ 
+			"err": err.Error(),
+		})
+		return
+	}
+
+	_, err = db.Exec(context.Background(), "insert into tododb (title) values($1)", todo.Title)
+	if err != nil { 
+		panic(err.Error())
+	}
+}
