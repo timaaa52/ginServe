2c0755f849492e1c06bc8bb09c676af6d15feb38
Tim <haukusha1@gmail.com>
2026-07-23 23:18:16 +0300

    create type for request params update service's

--- models/models.go
+++ models/models.go
@@ -8,3 +8,9 @@ type Todo struct {
 	IsDone    bool   `json:"isdone"`
 	AddedDate time.Time `json:"added_date"`
 }
+
+
+type RequestParams struct { 
+		Title string `json:"title" binding:"required"`
+		IsDone bool `json:"isdone" binding:"required"`
+	}
